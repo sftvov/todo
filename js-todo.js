e0d6401de3eb9448cdc42e5c2826ae3db328fbac
sftvov <SUrabota@mail.ru>
2020-07-20 10:57:59 +0300

    add function of button

--- js-todo.js
+++ js-todo.js
@@ -1,38 +1,15 @@
-let gameField = [
-    [null, null, null],
-    [null, null, null],
-    [null, null, null],
-]
-let i = 1;
-$('.row button').on('click', event => {
-    let x = event.target.dataset.btn;
-    let y = event.target.parentElement.dataset.row;
-    let activeButton = $(`[data-row="${y}"] [data-btn="${x}"]`);
-    if (activeButton[0].innerText == "") {
-        if (i%2 == 1 ) {
-            gameField[x][y] = 'x';
-            i++;
-        }
-        else {
-            gameField[x][y] = 'o';
-            i++;
-        }    
-        activeButton.text(gameField[x][y]);
+function pressBtn() {
+    if (!btnPlus.classList.contains('todo-save-button')) {     
+        inptTasks.classList.add('active-inline');
+        btnPlus.innerText = 's';
+        btnPlus.classList.add('todo-save-button');
     }
     else {
-        alert('error');
+        inptTasks.classList.remove('active-inline');
+        btnPlus.innerText = '+';
+        btnPlus.classList.remove('todo-save-button');
     }
-    
-    for (let i = 0; i < gameField.length; i++) {
-        const row = gameField[i];    
-        let arrBtn = [];    
-        for (let j = 0; j < row.length; j++) {
-            arrBtn.push(row[j]);
-        }
-        if (arrBtn[0] !== null && arrBtn[0] == arrBtn[1] && arrBtn[1] == arrBtn[2] ) {
-            alert(`${arrBtn[0]} win`);
-        }        
-    }
-});
-
-// Сделали крестики нолик для одного с победой в случае вертикального совпадения
+}
+let btnPlus = document.querySelector('.todo-add-button');
+btnPlus.addEventListener('click', pressBtn);
+let inptTasks = document.querySelector('.todo-input');
