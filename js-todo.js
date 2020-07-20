1502787d2dc7ec3771897ae50575fbd89a65f506
sftvov <63662363+sftvov@users.noreply.github.com>
2020-07-20 22:17:29 +0300

    little +

--- js-todo.js
+++ js-todo.js
@@ -8,7 +8,7 @@ function pressBtn() {
         inptTasks.classList.remove('active-inline');
         btnPlus.innerText = '+';
         btnPlus.classList.remove('todo-save-button');
-        nameNewTask = inptTasks.children[0].value;
+        let nameNewTask = inptTasks.children[0].value;
         let newItem = document.createElement('div');
         newItem.classList.add('todo-item');
         newItem.innerHTML = `<div class="todo-item__checkbox">
@@ -20,11 +20,12 @@ function pressBtn() {
         </div>
     </div>`;
         document.querySelector('.todo-items').append(newItem);
-        console.log(nameNewTask);
+        arrOfTasks.push(nameNewTask);
+        localStorage.setItem('tasks', JSON.stringify(arrOfTasks));
     }
 }
 
 let btnPlus = document.querySelector('.todo-add-button');
 btnPlus.addEventListener('click', pressBtn);
 let inptTasks = document.querySelector('.todo-input');
-let nameNewTask;
+let arrOfTasks = JSON.parse(localStorage.getItem('tasks'));
