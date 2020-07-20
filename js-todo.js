628585832695fe45629eea079885fc887aeecce5
sftvov <SUrabota@mail.ru>
2020-07-20 11:30:06 +0300

    ++

--- js-todo.js
+++ js-todo.js
@@ -8,8 +8,23 @@ function pressBtn() {
         inptTasks.classList.remove('active-inline');
         btnPlus.innerText = '+';
         btnPlus.classList.remove('todo-save-button');
+        nameNewTask = inptTasks.children[0].value;
+        let newItem = document.createElement('div');
+        newItem.classList.add('todo-item');
+        newItem.innerHTML = `<div class="todo-item__checkbox">
+        <input type="checkbox">
+    </div>
+    <div class="todo-item__description">
+        <div class="todo-item__title">
+            ${nameNewTask}
+        </div>
+    </div>`;
+        document.querySelector('.todo-items').append(newItem);
+        console.log(nameNewTask);
     }
 }
+
 let btnPlus = document.querySelector('.todo-add-button');
 btnPlus.addEventListener('click', pressBtn);
 let inptTasks = document.querySelector('.todo-input');
+let nameNewTask;
